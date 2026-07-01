4777f407589bfb4c3bd4415d03ca626477073b2a
Mahnoor Rizwan <rizwan.mahnoor1905@gmail.com>
2026-07-01 11:26:52 +0500

    Delete Beginner/Beginner/02_Rock_Paper_Scissors.py

--- Beginner/Beginner/02_Rock_Paper_Scissors.py
+++
@@ -1,5 +0,0 @@
-import random
-choice=['rock','paper','scissor']
-user_choice=input('enter your choice').lower()
-if user_choice in choice:
-       print(f"computer choice is {random.choice(choice)}")
